8afe84a6ec57059158c722df8a6f9c9f22ea0c9d
Nour Al Salhi <nuraym9998@gmail.com>
2026-01-19 19:05:30 +0200

    add inventory model

---
+++ src/db/models/inventory.model.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../../config/database';
+import Product from './product.model';
+import Location from './location.model';
+
+interface InventoryAttributes {
+  id: number;
+  product_id: number;
+  location_id: number;
+  quantity: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface InventoryCreationAttributes extends Optional<InventoryAttributes, 'id'> {}
+
+class Inventory extends Model<InventoryAttributes, InventoryCreationAttributes>
+  implements InventoryAttributes {
+  public id!: number;
+  public product_id!: number;
+  public location_id!: number;
+  public quantity!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Inventory.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Product, key: 'id' },
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Location, key: 'id' },
+    },
+    quantity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  },
+  {
+    sequelize,
+    tableName: 'inventory',
+    timestamps: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ['product_id', 'location_id'],
+      },
+    ],
+  }
+);
+
+export default Inventory;
